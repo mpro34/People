858c045d6fb40ad13b753f2fda5bdbf198b33283
Chris Whiting <chriswhiting@Chriss-MacBook-Pro.local>
2018-01-08 19:21:28 -0800

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -13,6 +13,19 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
+  //socket.emit from admin text: Welcome to the chat app
+  //socket.broadcast.emit frtom Admin text new user joined
+
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app'
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    test: 'New user joined!',
+    createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
@@ -21,6 +34,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
